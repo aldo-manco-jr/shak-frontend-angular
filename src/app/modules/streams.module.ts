7afda47955eaf4c5f028f98801ecf754b5d6ed12
aldo-manco-jr <aldo.manco@protonmail.ch>
2020-07-14 13:11:20 +0200

    'frontend_side_posts'

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import {ToolbarComponent} from "../components/toolbar/toolbar.component";
+import { SideComponent } from '../components/side/side.component';
+import { PostFormComponent } from '../components/post-form/post-form.component';
+import { PostsComponent } from '../components/posts/posts.component';
 
 
 
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent],
   imports: [CommonModule],
   exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
