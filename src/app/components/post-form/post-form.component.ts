5e04f8bfd8bcfc34c3c644d3be47642b2fb7e5ff
Frank <ledkilt@gmail.com>
2020-07-15 13:47:43 +0200

    'token_maledetto'

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,6 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import {PostService} from "../../services/post.service";
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private postService: PostService) { }
 
   ngOnInit(): void {
+    this.init();
   }
 
+  init(){
+    this.postForm = this.fb.group({
+      post: ['', Validators.required]
+    });
+  }
+
+  submitPost(){
+  this.postService.addPost(this.postForm.value).subscribe(data => {
+    console.log(data);
+  });
+  }
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const BASE_URL = 'http://localhost:3000/api/shak'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) { }
+
+
+  addPost(body): Observable<any> {
+    //interpolazione della stringa
+    return this.http.post(BASE_URL + "/post/add-post", body);
+  }
+}
